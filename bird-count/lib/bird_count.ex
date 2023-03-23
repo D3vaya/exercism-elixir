3a19ca29bceb9a5ea1dfd5ec6da841730fb46332
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-23 16:48:21 -0300

    feat: new challnge

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -1,21 +1,33 @@
 defmodule BirdCount do
   def today(list) do
-    # Please implement the today/1 function
+    if Enum.empty?(list) do
+      nil
+    end
+
+    List.first(list)
   end
 
   def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
+    if Enum.empty?(list) do
+      [1]
+    else
+      list |> List.update_at(0, &(&1 + 1))
+    end
   end
 
   def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+    Enum.member?(list, 0)
   end
 
   def total(list) do
-    # Please implement the total/1 function
+    Enum.sum(list)
   end
 
   def busy_days(list) do
-    # Please implement the busy_days/1 function
+    if Enum.empty?(list) do
+      0
+    else
+      Enum.filter(list, fn n -> n > 4 end) |> Enum.count()
+    end
   end
 end
